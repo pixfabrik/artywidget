d8ea95711b7ec1ce6c2e3906ed513b6ebd4bdb93
Ian Gilman <ian@iangilman.com>
2017-09-20 16:26:21 -0700

    Testing

--- src/env-helpers.js
+++ src/env-helpers.js
@@ -4,6 +4,7 @@ var isProd = (ipAddress !== 'localhost');
 var wsPort = (ipAddress === 'localhost') ? 16433 : 8080;
 var baseUrl = (isProd) ? 'http://www.artywidget.com' : 'http://' + ipAddress;
 baseUrl = 'http://artywidget.us-3.evennode.com';
+console.log('!!!', baseUrl);
 
 if (!isProd && port !== 80) {
   baseUrl += ':' + port;
